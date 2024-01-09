d9fbe48315f6ed602a4402ecf030a15ce51d5f01
abel-udoh <abeludoh8@gmail.com>
2024-01-09 18:48:02 +0100

    user authentication successfully set up

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,5 @@
+
+const path = require('path');
 const { insertUser, findUserByEmail } = require('../models/Users');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
@@ -14,6 +16,11 @@ const UserController = {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
+      // Check if the email already exists
+      const existingUser = await findUserByEmail(email);
+      if (existingUser) {
+        return res.status(400).send('Email already exists');
+      }
       await insertUser(email, hashedPassword);
       res.send('User registered successfully');
     } catch (error) {
@@ -23,35 +30,39 @@ const UserController = {
   },
 
   login: async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-      const user = await findUserByEmail(email);
+  try {
+    const user = await findUserByEmail(email);
 
-      if (!user) {
-        return res.send('Invalid email or password');
-      }
-
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (isPasswordValid) {
-        req.session.user = user;
-        res.redirect('/dashboard');
-      } else {
-        res.send('Invalid email or password');
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      res.status(500).send('Internal Server Error');
+    if (!user) {
+      return res.send('Invalid email or password');
     }
-  },
 
-  dashboard: (req, res) => {
-    if (req.session.user) {
-      res.send('Welcome to the dashboard');
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      // Store user email in localStorage
+      //localStorage.setItem('userEmail', email);
+      req.session.user = user;
+      req.session.email = user.email; // Store email in the session
+      res.redirect('/dashboard');
     } else {
-      res.redirect('/login');
+      res.send('Invalid email or password');
     }
-  },
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).send('Internal Server Error');
+  }
+},
+
+dashboard: (req, res) => {
+  if (req.session.user) {
+    res.sendFile(path.join(__dirname, '../public/dashboard.html'));
+  } else {
+    res.redirect('/login');
+  }
+},
+
 
   logout: (req, res) => {
     req.session.destroy();
