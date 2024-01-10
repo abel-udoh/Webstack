49e1dfb9c31977ba56b8039bb3f70303a2c4daa4
abel-udoh <abeludoh8@gmail.com>
2024-01-10 17:01:25 +0100

    Update port configuration for Heroku

--- server.js
+++ server.js
@@ -23,9 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 connectToDatabase()
   .then(() => {
     app.use('/', routes);
+    const PORT = process.env.PORT || 3000; // Use dynamic port for Heroku
 
-    app.listen(3000, () => {
-      console.log('Server started on port 3000');
+    /*app.listen(3000, () => {
+      console.log('Server started on port 3000');*/
+      app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
     });
   })
   .catch(err => {
